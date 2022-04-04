db2fd8beb2fffbc0a947f381e77ab4226b8a3e99
Anthony Rodríguez N <rodriguezanthony981@gmail.com>
2022-04-04 16:07:27 -0500

    Exercises 1.3 Complete

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,59 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = ({course}) => <h1>{course}</h1>
-const Content = () => {
-  const Lists = List.map( 
-    function(List){
-      return (
-        <div key={List.exercises.toString()}>
-          <Part part={List.part} exercises={List.exercises} />
-        </div>
-      )
-    }
-  )
-  return Lists
-}
-
 const Part = ({part, exercises}) => {
   return <>
     <p>{part}  {exercises}</p>
   </>
 }
 
-const List = [
-  {id: 1,
-  part: 'Fundamentals of React',
-  exercises: 10
-  },
-  {id: 2,
-  part: 'Using props to pass data',
-  exercises: 7
-  },
-  {id: 3,
-  part:'State of a component',
-  exercises: 14
-  }
-]
-const Total = () => {
-  const L = List.map(function(List){ 
-      return (List.exercises)
-    }
-  )
-  const R = L[0] + L[1] + L[2]
-
-  return  <p> Number of exercises {R}</p>
-}
-
-
 const App = () => {
   const course = 'Half Stack application development'
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
-      <Content></Content>
+      <Part part={part1.name} exercises={part1.exercises}/>
+      <Part part={part2.name} exercises={part2.exercises}/>
+      <Part part={part3.name} exercises={part3.exercises}/>
+      <p>Number of exercises:{part1.exercises+part2.exercises+part3.exercises}</p>
 
-      <Total />
     </div>
   )
 }
